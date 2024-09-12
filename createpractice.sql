b711ae7b471faf04b139c70b7e7a5b4674b0a4bd
ArielleWaks <ariellemwaks@gmail.com>
2024-09-12 12:46:02 -0500

    insert into, insert into select statements

--- createpractice.sql
+++ createpractice.sql
@@ -13,7 +13,7 @@ ADD Status VARCHAR(255) DEFAULT 'Not Started';
 
 -- Create table with constraints
 CREATE TABLE Persons (
-    ID int UNIQUE NOT NULL,
+    ID int UNIQUE NOT NULL AUTO_INCREMENT,
     LastName varchar(255) UNIQUE NOT NULL,
     FirstName varchar(255),
     Age int,
@@ -30,3 +30,17 @@ CREATE TABLE OldPersons AS
     SELECT LastName, FirstName, Age
     FROM Persons
     WHERE Age > 50;
+
+
+-- Insert Into statement with multiple values
+INSERT INTO Persons (LastName, FirstName, Age, Email)
+VALUES
+    ('Waks', 'Arielle', 30, 'aw@aol.com'),
+    ('Witherspoon', 'Theo', 30, 'tw@hotmail.com'),
+    ('Sontag', 'Ruth', 60, 'rs@yearthlink.net');
+
+-- Insert Into Select statement
+INSERT INTO OldPersons
+SELECT LastName, FirstName, Age FROM Persons
+WHERE Age > 50;
+
